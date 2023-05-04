af0cc649f52c65e1db726349fa730166da448dd0
inerer <arshinov04@gmail.com>
2023-05-05 00:20:33 +0300

    bebasjdb

--- PhotoStudio/Pages/AddEditSupplyPage.xaml.cs
+++ PhotoStudio/Pages/AddEditSupplyPage.xaml.cs
@@ -7,29 +7,59 @@ namespace PhotoStudio.Pages;
 
 public partial class AddEditSupplyPage : Page
 {
-    private TypeSupply _typeSupply;
-    private TypeSupplyService _typeSupplyService;
+    private readonly TypeSupply _typeSupply;
+    private readonly TypeSupplyService _typeSupplyService;
+    private readonly Supply _supply;
+    private readonly Rent _rent;
+    private readonly Hall _hall;
+    private readonly SupplyService _supplyService;
     public AddEditSupplyPage()
     {
         InitializeComponent();
-       
         _typeSupply = new TypeSupply();
         _typeSupplyService = new TypeSupplyService();
+        _supply = new Supply();
+        _rent = new Rent();
+        _hall = new Hall();
+        _supplyService = new SupplyService();
         ComboBoxRendered();
-    }
-
-    private void DescriptionSupplyTextBox_OnSelectionChanged(object sender, RoutedEventArgs e)
-    {
-        throw new System.NotImplementedException();
     }
 
     private void RentComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        _supply.Rent = (Rent)RentComboBox.SelectedItem;
     }
 
     private void ComboBoxRendered()
     {
         TypeSupplyComboBox.ItemsSource = _typeSupplyService.GetAllTypeSupplies(_typeSupply);
+        
+        RentComboBox.Visibility = Visibility.Collapsed;
+        AddRentButton.Visibility = Visibility.Collapsed;
+
+        RentComboBox.ItemsSource = _supplyService.GetAllSupplies(_supply);
+
+    }
+
+    private void TypeSupplyComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        _supply.TypeSupply = (TypeSupply)TypeSupplyComboBox.SelectedItem;
+
+        if (_supply.TypeSupply.Id == 1)
+        {
+            RentComboBox.Visibility = Visibility.Visible;
+            AddRentButton.Visibility = Visibility.Visible;
+        }
+           
+    }
+
+    private void AddTypeSupplyButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AddRentButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
